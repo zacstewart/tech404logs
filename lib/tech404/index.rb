937733458682ecc761ab51084f2fbc5ff4ffe80f
Zac Stewart <zgstewart@gmail.com>
2015-10-07 07:40:17 -0400

    Add better logging

--- lib/tech404/index.rb
+++ lib/tech404/index.rb
@@ -1,11 +1,9 @@
-require 'pp'
 require 'data_mapper'
-require 'eventmachine'
-require 'faye/websocket'
+require 'logger'
 require 'sinatra/base'
-require 'slack/api'
 require 'tech404/index/application'
 require 'tech404/index/channel'
+require 'tech404/index/connection'
 require 'tech404/index/message'
 require 'tech404/index/message_handler'
 require 'tech404/index/user'
@@ -18,62 +16,8 @@ DataMapper.auto_upgrade!
 
 module Tech404
   module Index
-    class Connection
-      def initialize(token = ENV['SLACK_TOKEN'])
-        @token = token
-        @reactor = EM
-        @ws = nil
-      end
-
-      def run
-        sync_channels
-        sync_users
-
-        reactor.run do
-          @ws = Faye::WebSocket::Client.new(url, [])
-          ws.onopen = method(:on_open)
-          ws.onclose = method(:on_close)
-          ws.onmessage = method(:on_message)
-        end
-      end
-
-      def on_open(event)
-        pp "ohai"
-      end
-
-      def on_message(message)
-        reactor.defer { MessageHandler.handle(message) }
-      end
-
-      def on_close(close)
-        pp("connection closed: #{close.code}, #{close.reason}")
-        reactor.stop
-        @ws = nil
-      end
-
-      private
-
-      attr_reader :reactor, :token, :ws
-
-      def url
-        rtm.fetch('url')
-      end
-
-      def rtm
-        @rtm ||= Slack::Api.new(token).rtm_start
-      end
-
-      def sync_channels
-        rtm.fetch('channels').each do |channel|
-          Channel.create_or_update(channel)
-        end
-      end
-
-      def sync_users
-        rtm.fetch('users').each do |user|
-          User.store(user)
-        end
-      end
+    def self.logger
+      @logger ||= Logger.new(STDOUT)
     end
   end
 end
