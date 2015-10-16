69f36de99177d1841e4841a8b18c8f90b6fecfaf
Zac Stewart <zgstewart@gmail.com>
2015-10-15 20:39:54 -0400

    Add test for channel mention filter

---
+++ test/tech404/index/channel_mention_filter_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+describe Tech404::Index::ChannelMentionFilter do
+  let(:channels) { mock }
+  let(:general) { mock(name: 'general') }
+  subject { Tech404::Index::ChannelMentionFilter.new(text, channel_repo: channels) }
+
+  describe '#apply' do
+    describe 'when the mention includes a name' do
+      let(:text) { 'Go back to <#C1234|general>' }
+
+      it 'formats the message to include the channel name' do
+        subject.apply.must_equal('Go back to #general')
+      end
+    end
+
+    describe 'when the mention does not include a name' do
+      let(:text) { 'Go back to <#C1234>' }
+
+      it 'finds the channel by id to get the name' do
+        channels.expects(:get).with('C1234').returns(general)
+        subject.apply.must_equal('Go back to #general')
+      end
+    end
+  end
+end
