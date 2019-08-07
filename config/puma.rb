17b7c1dff074f39d394fa59b4f6594d05637c861
Zac Stewart <zgstewart@gmail.com>
2019-08-06 22:49:35 -0400

    Spaceing

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@ threads threads_count, threads_count
 preload_app!
 
 rackup DefaultRackup
-port ENV['PORT']     || 3000
+port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 before_fork do
