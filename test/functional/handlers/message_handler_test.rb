4115072f631c2218c5e4d422126f330a262ef335
Zac Stewart <hi@zacstewart.com>
2022-03-22 14:37:18 +0800

    Use Sequel tables to test message handler
    Break the dependency on DataMapper models

--- test/functional/handlers/message_handler_test.rb
+++ test/functional/handlers/message_handler_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 describe Handlers::MessageHandler do
   subject { Handlers::MessageHandler.new }
+  let(:messages) { Sequel::Model.db[:messages] }
+  let(:users) { Sequel::Model.db[:users] }
 
   describe '#handle' do
     let(:event) { {
@@ -21,17 +23,18 @@ describe Handlers::MessageHandler do
     it 'inserts a Message' do
       returned_id = subject.handle(event)
 
-      message = Message.first(id: returned_id)
-      message.id.must_equal(returned_id)
-      message.channel_id.must_equal('C123')
-      message.user_id.must_equal('U123')
-      message.text.must_equal('Hello')
-      message.timestamp.must_equal(DateTime.parse('2019-08-03T14:11:39-04:00'))
+      message = messages.where(id: returned_id).first
+      _(message[:id]).must_equal(returned_id)
+      _(message[:channel_id]).must_equal('C123')
+      _(message[:user_id]).must_equal('U123')
+      _(message[:text]).must_equal('Hello')
+      _(message[:timestamp].utc.iso8601).must_equal(
+        Time.new(2019, 8, 3, 18, 11, 39.55599).utc.iso8601)
     end
 
     it 'upserts a User' do
       subject.handle(event)
-      User.first(id: 'U123').wont_equal(nil)
+      _(users.where(id: 'U123').count).must_equal(1)
     end
   end
 end
